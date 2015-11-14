3ae278d42ca93717286a9c635d02ef03375f1978
ben-hur batista oliveira <hifenhur@mac-mini-de-ben-hur.local>
2015-11-13 22:12:21 -0200

    initial

--- iRadio/ViewController.swift
+++ iRadio/ViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import MediaPlayer
+import AVKit
 
 class ViewController: UIViewController {
-
+    var player : AVPlayer = AVPlayer()
+    var playing = true;
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let videoURL = NSURL(string: "http://radio.livesh.com.br:8144")
+        player = AVPlayer(URL: videoURL!)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(playerLayer)
+        player.play()
+        print("is playing")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func PlayAction() {
+        if (playing){
+            player.pause()
+        }else{
+            player.play()
+        }
+        playing = !playing
+        
     }
 
 
